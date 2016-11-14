31dca5093b3223429e6065aefdfed48e7d153f26
root <root@robbie-desktop.pes.local>
2016-11-14 09:34:31 -0500

    Begin restore.sh

---
+++ restore.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+start=`date +%s`
+
+# Don't allow the script to run if it's already running. May occur if your logs or config take longer than 5 minutes to backup.
+if pidof -o %PPID -x "`basename "$0"`">/dev/null; then
+    echo "Process already running"
+    exit
+fi
+
+if [[ $1 = "" ]]; then
+  echo "Usage: sudo ./restore.sh /location/of/backup.nems"
+  exit
+fi
+
+if [[ $EUID -ne 0 ]]; then
+  echo "ERROR: You must be a root" 2>&1
+  exit 1
+else
+
+	if [[ -e $1 ]]; then
+
+		echo Let me be VERY clear here...
+		echo This will WIPE OUT the configuration on this NEMS deployment.
+		echo The configuration will be replaced with the one stored in your NEMS backup.
+	 
+		read -r -p "Are you sure? [y/N] " response
+		if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+			 
+				cd /tmp
+
+				 tar -zxf $1
+
+				 end=`date +%s`
+
+				 runtime=$((end-start))
+
+
+				 if [ -d /var/log/nems ]
+					 then
+						 echo $runtime > /var/log/nems/migrator-restore-runtime.log
+					 else
+						 mkdir /var/log/nems
+						 echo $runtime > /var/log/nems/migrator-restore-runtime.log
+				 fi
+				 
+		else
+				echo Aborted.
+		fi
+		 
+	else
+	  echo $1 does not exist. Aborting.
+	fi
+	 
+fi
