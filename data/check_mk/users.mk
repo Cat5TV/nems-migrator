546e580d9e9b7863104be6815428aa6ee55e5069
Cat5TV <contact@category5.tv>
2017-06-07 12:49:52 +0000

    Fix Check_MK User Access

---
+++ data/check_mk/users.mk
@@ -0,0 +1,10 @@
+# Written by Multisite UserDB
+# encoding: utf-8
+
+multisite_users = \
+{u'nagiosadmin': {'alias': u'nagiosadmin',
+                  'force_authuser': False,
+                  'force_authuser_webservice': False,
+                  'locked': False,
+                  'roles': ['admin'],
+                  'start_url': 'dashboard.py'}}
