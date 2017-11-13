84585b3933c31deb546e5083c653316bd8b2ee74
Cat5TV <contact@category5.tv>
2017-11-13 14:08:13 -0500

    Setup 1.3 compatibility

--- restore.sh
+++ restore.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Remove restore functionality from legacy versions of NEMS
-ver=$(/usr/local/bin/nems-info nemsver) 
+ver=$(/usr/local/bin/nems-info nemsver)
 if (( ! $(awk 'BEGIN {print ("'$ver'" >= "'1.2.1'")}') )); then
    echo "ERROR: nems-restore requires NEMS 1.2.1 or higher"
    exit
@@ -38,49 +38,45 @@ else
 		echo Please do this on a fresh deployment of NEMS to prevent data loss.
 		echo "I am not responsible for this script breaking everything you have done :)"
 		echo Backup, backup, backup.
-		
+
 		echo ""
-	 
+
 		read -r -p "Are you sure you want to attempt restore? [y/N] " response
-		if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
 
 				mkdir -p /tmp/nems_migrator_restore
 				cd /tmp/nems_migrator_restore
 
 				 tar -zxf $1
-				 
+
 				# Legacy compatibility
 				if [[ -f "/tmp/nems_migrator_restore/var/www/html/inc/ver.txt" ]]; then
 				 backupver=$(cat "/tmp/nems_migrator_restore/var/www/html/inc/ver.txt")
 
 				 # Current nems.conf version storage
-				 else if [[ -f "/tmp/nems_migrator_restore/usr/local/share/nems/nems.conf" ]]; then
+				 elif [[ -f "/tmp/nems_migrator_restore/usr/local/share/nems/nems.conf" ]]; then
 				   backupver=$(cat /tmp/nems_migrator_restore/usr/local/share/nems/nems.conf | grep version |  printf '%s' $(cut -n -d '=' -f 2))
 
-                                 # NEMS 1.2.x old storage location (moved with fixes.sh)
-				 else if [[ -f "/tmp/nems_migrator_restore/home/pi/nems.conf" ]]; then
+         # NEMS 1.2.x old storage location (moved with fixes.sh)
+				 elif [[ -f "/tmp/nems_migrator_restore/home/pi/nems.conf" ]]; then
 				   backupver=$(cat /tmp/nems_migrator_restore/home/pi/nems.conf | grep version |  printf '%s' $(cut -n -d '=' -f 2))
 
-					 # We don't really know the true version, but we know this is from NEMS, so set 1.2
-					 else if [[ -d "/var/log/nems/" ]]; then
-					   backupver=1.2
-					   
-				   fi
-				   
-				 fi
+				 # We don't really know the true version, but we know this is from NEMS, so set 1.2
+				 elif [[ -d "/var/log/nems/" ]]; then
+					 backupver=1.2
 
-				fi
-				 
-				 ver=$(/usr/local/bin/nems-info nemsver) 
-				 
-				 if (( ! $(awk 'BEGIN {print ("'$backupver'" >= "'1.0'")}') )); then
+        fi
+
+				 ver=$(/usr/local/bin/nems-info nemsver)
+
+				 if (( ! $(awk 'BEGIN {print ("$backupver" >= "'1.0'")}') )); then
 				   echo Backup file is from NEMS $backupver. Proceeding.
 				   service nagios3 stop
-				   
+
 				   # I know I warned you, but I love you too much to let you risk it.
 				   /root/nems/nems-migrator/backup.sh > /dev/null 2>&1
-				   cp -p /var/www/html/backup/backup.nems /root/
-				   
+				   cp -p /var/www/html/backup/snapshot/backup.nems /root/
+
 				   if [[ -d "/tmp/nems_migrator_restore/etc/nagios3" ]]; then
 
                                          # Clobber the existing configs which will not be consolidated
@@ -89,31 +85,29 @@ else
 
                                          # Reconcile and clobber all other config files
                                          /root/nems/nems-migrator/data/reconcile-nagios.sh
-					 
+
 					 # Clear MySQL database and import new consolidated configs into NConf
 					 /root/nems/nems-migrator/data/nconf-import.sh
 
                                          # Activate default nagios monitor on all hosts
                                          /root/nems/nems-migrator/data/nconf-activate.sh
 
-                                   else 
+            else
                                          echo "Nagios Configuration Missing. This is a critical error."
                                          exit
-                                   fi
+           fi
 
 
 					 if [[ $backupver == "1.0" ]]; then
 					 	echo "Upgrading to newer version of NEMS: Please edit /etc/nagios3/resource.cfg to configure your settings."
-					 else
-						 if [[ -e "/tmp/nems_migrator_restore/etc/nagios3/resource.cfg" ]]; then
+					  elif [[ -e "/tmp/nems_migrator_restore/etc/nagios3/resource.cfg" ]]; then
 							 /root/nems/nems-migrator/data/reconcile-resource.sh
-						  else 
-							 echo "Nagios Configuration Missing. This is a critical error."
-							 exit
-						 fi
+						else
+						  echo "Nagios Configuration Missing. This is a critical error."
+							exit
 					 fi
-				   
-				   
+
+
 					 # NagVis maps are stored differently in NEMS 1.0
 					 if [[ $backupver == "1.0" ]]; then
 							nagvissrc="maps/"
@@ -128,25 +122,25 @@ else
 				   if [[ -d "/tmp/nems_migrator_restore/etc/nagvis/$nagvissrc" ]]; then
 						 rm -rf /etc/nagvis/$nagvisdest\maps
 						 cp -Rp /tmp/nems_migrator_restore/etc/nagvis/$nagvissrc /etc/nagvis/$nagvisdest
-				   else 
+				   else
 							 echo "NagVis failed. Your NagVis data is corrupt."
 					 fi
-					 
+
 					 if [[ -d "/tmp/nems_migrator_restore/var/www/nconf/output" ]]; then
 						 rm -rf /var/www/nconf/output/
 						 cp -Rp /tmp/nems_migrator_restore/var/www/nconf/output /var/www/nconf/
-				   else 
+				   else
 							 echo "NConf failed. Your NConf data is corrupt."
 							 echo "You should be able to re-create it."
 					 fi
-					
-				   
+
+
 				   # This may cause errors, but at least it gives them the old logs.
 				   cp -Rp /tmp/nems_migrator_restore/var/log/* /var/log
-				   
+
 				   service mysql start
 				   service nagios3 start
-				   
+
 				   echo ""
 				   echo I hope everything worked okay for you.
 				   echo Please let me know if you had any trouble.
@@ -154,30 +148,30 @@ else
 				   echo PS - I saved a backup for you of the old config. /root/backup.nems
 				   echo      ... just in case
 				   echo ""
-				   
+
 				 else
 				   echo Your backup file is either invalid, or an unsupported version. Aborted.
 				 fi
 
 				 #rm -rf /tmp/nems_migrator_restore
-				 
+
 				 end=`date +%s`
 				 runtime=$((end-start))
 
-				 if [ -d /var/log/nems ]
-					 then
+				 if [[ -d "/var/log/nems" ]]; then
 						 echo $runtime > /var/log/nems/migrator-restore-runtime.log
 					 else
 						 mkdir /var/log/nems
 						 echo $runtime > /var/log/nems/migrator-restore-runtime.log
 				 fi
-				 
+
 		else
 				echo Aborted.
 		fi
-		 
+
 	else
 	  echo $1 does not exist. Aborting.
 	fi
-	 
+
 fi
+
