3fdc3983f3440a32df2f0459489aa2d2df44592b
Cat5TV <contact@category5.tv>
2018-01-22 14:49:44 -0500

    Offsite Backup works

--- offsite-backup.sh
+++ offsite-backup.sh
@@ -6,11 +6,10 @@
 
 # Load Config
 hwid=`/usr/local/bin/nems-info hwid`
-apikey=$(cat /usr/local/share/nems/nems.conf | grep apikey | printf '%s' $(cut -n -d '=' -f 2))
-password=$(cat /usr/local/share/nems/nems.conf | grep password | printf '%s' $(cut -n -d '=' -f 2))
-obskey=$(cat /usr/local/share/nems/nems.conf | grep obskey | printf '%s' $(cut -n -d '=' -f 2))
+osbpass=$(cat /usr/local/share/nems/nems.conf | grep osbpass | printf '%s' $(cut -n -d '=' -f 2))
+osbkey=$(cat /usr/local/share/nems/nems.conf | grep osbkey | printf '%s' $(cut -n -d '=' -f 2))
 
-if [[ $apikey == '' ]] || [[ $password == '' ]] || [[ $obskey == '' ]]; then
+if [[ $osbpass == '' ]] || [[ $osbkey == '' ]]; then
   echo NEMS Migrator Offsite Backup is not currently enabled.
   exit
 fi;
@@ -18,7 +17,8 @@ fi;
 # Should wrap this bit in a check to the API to see if this user is authorized. No point in uploading if not.
 
 # Encrypt the file
-gpg --yes --batch --passphrase="$password" -c /var/www/html/backup/snapshot/backup.nems
+# Combine the user's passphrase with the OSB Key to further strenghten the entropy of the passphrase
+gpg --yes --batch --passphrase="::$osbpass::$osbkey::" -c /var/www/html/backup/snapshot/backup.nems
 
 # Upload the file
-curl -F "hwid=$hwid" -F "apikey=$apikey" -F "backup=@/var/www/html/backup/snapshot/backup.nems.gpg" https://nemslinux.com/api/offsite-backup/
+curl -F "hwid=$hwid" -F "osbkey=$osbkey" -F "backup=@/var/www/html/backup/snapshot/backup.nems.gpg" https://nemslinux.com/api/offsite-backup/
