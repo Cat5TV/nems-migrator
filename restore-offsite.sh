7c8d9a7efb0fbe2d0f370041806899ae7b7a48f7
Cat5TV <contact@category5.tv>
2018-01-26 13:28:53 -0500

    Offsite Backup Restore Feature

---
+++ restore-offsite.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+# NEMS Linux Migrator Off-Site Backup Restore
+# By Robbie Ferguson
+# nemslinux.com | baldnerd.com | category5.tv
+
+# Load Config
+hwid=`/usr/local/bin/nems-info hwid`
+osbpass=$(cat /usr/local/share/nems/nems.conf | grep osbpass | printf '%s' $(cut -n -d '=' -f 2))
+osbkey=$(cat /usr/local/share/nems/nems.conf | grep osbkey | printf '%s' $(cut -n -d '=' -f 2))
+timestamp=$(/bin/date +%s)
+
+if [[ $osbpass == '' ]] || [[ $osbkey == '' ]]; then
+  echo NEMS Migrator Offsite Backup is not currently enabled.
+  exit
+fi;
+
+# Load Account Data
+  data=$(curl -s -F "hwid=$hwid" -F "osbkey=$osbkey" https://nemslinux.com/api-backend/offsite-backup-checkin.php)
+  set -f
+  menu=''
+  counter=0
+  NEWLINE=$'\n'
+  for line in $data; do
+    let counter+=1
+    datarr=(${line//::/ })
+    serverdate="${datarr[0]}"
+    filedate="${datarr[1]}"
+    filelocaldate="${datarr[2]}"
+    filesize="${datarr[3]}"
+    menu="$menu$counter $filelocaldate${NEWLINE}"
+  done
+
+HEIGHT=25
+WIDTH=25
+CHOICE_HEIGHT=25
+BACKTITLE="NEMS Linux"
+TITLE="Off-Site Backup"
+MENU="Download Backup:"
+
+OPTIONS=($menu)
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+if test $? -eq 0
+then 
+  clear
+else
+  clear
+  echo "Canceled"
+  exit
+fi
+
+  counter=0
+  for line in $data; do
+    let counter+=1
+    if [[ $CHOICE == $counter ]]; then
+      datarr=(${line//::/ })
+      serverdate="${datarr[0]}"
+      filelocaldate="${datarr[2]}"
+      echo "Downloading NEMS Migrator Off-Site Backup Server for backup from $filelocaldate..."
+      echo ""
+      curl -F "hwid=$hwid" -F "osbkey=$osbkey" -F "date=$serverdate" https://nemslinux.com/api-backend/offsite-backup-restore.php -o /tmp/backup.nems.gpg
+      echo ""
+      echo "Attempting decryption..."
+      echo ""
+      gpg --yes --batch --passphrase="::$osbpass::$osbkey::" --decrypt /tmp/backup.nems.gpg > /tmp/backup.nems
+      if [ -f /tmp/backup.nems.gpg ]; then
+        rm /tmp/backup.nems.gpg
+      fi;
+      if ! tar -tf /tmp/backup.nems &> /dev/null; then
+        echo Error with backup.
+        exit
+      else
+        echo ""
+        echo "Successfully decrypted. Running nems-restore..."
+        echo ""
+        nems-restore /tmp/backup.nems
+      fi
+    fi
+  done
+
+
+
