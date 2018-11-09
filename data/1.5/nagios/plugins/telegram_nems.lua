d459f836ddf31f9fc8b4afaa6e554d8bfa001752
NEMS Linux <nems@baldnerd.com>
2018-11-09 14:37:22 -0500

    Add NEMS Alias to Telegram, improve script

--- data/1.5/nagios/plugins/telegram_nems.lua
+++ data/1.5/nagios/plugins/telegram_nems.lua
@@ -1,7 +1,11 @@
 #!/usr/bin/lua
 https = require('ssl.https')
-local telegram_url = 'https://api.telegram.org/' .. arg[1] .. '/sendMessage?'
-local chat_id = arg[2]
+local handle = io.popen("/usr/local/bin/nems-info alias")
+local result = handle:read("*a")
+handle:close()
+local alias = string.gsub(result, "\n", "")
+local telegram_url = 'https://api.telegram.org/bot' .. arg[1] .. '/sendMessage?'
+local chat_id = '-' .. arg[2]
 
    Notification = "Notification Type: " .. arg[3] ..'\n'    --$NOTIFICATIONTYPE$
    Host = "Host: " .. arg[4] ..'\n'                         --$HOSTNAME$
@@ -9,12 +13,13 @@ local chat_id = arg[2]
    Address = "Address: " .. arg[6] ..'\n'                   --$HOSTADDRESS$
    Info = "Info: "  .. arg[7] ..'\n'                        --$HOSTOUTPUT$/"$SERVICEOUTPUT$"
    Date_Time = "Date/Time: " .. arg[8] ..'\n'               --$LONGDATETIME$
+   Alias = "Reporting NEMS Server: " .. alias .. '\n'       --NEMS Server Alias
 
    if (#arg == 8) then --assumes this since 8 arguments have been passed
-     message = '***** Nagios ***** ' ..'\n' ..'\n' .. Notification .. Host ..State .. Address .. Info .. Date_Time
+     message = '***** NEMS Linux ***** ' ..'\n' ..'\n' .. Notification .. Host ..State .. Address .. Info .. Date_Time .. Alias
    else 
      Service = "Service: " .. arg[8] .. '\n'                  --$SERVICEDESC$
-     message = '***** Nagios ***** ' ..'\n' ..'\n' .. Notification .. Host  .. Service ..State .. Address .. Info .. Date_Time
+     message = '***** NEMS Linux ***** ' ..'\n' ..'\n' .. Notification .. Host  .. Service ..State .. Address .. Info .. Date_Time .. Alias
    end
 
 local data_str = 'chat_id=' .. chat_id .. '&text=' .. message..''  
