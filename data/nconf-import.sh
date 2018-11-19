a543056ccc251b5422ce3c24fea06f9fc7209cf2
NEMS Linux <nems@baldnerd.com>
2018-11-19 10:44:15 -0500

    Automated clean DB for NEMS 1.5

--- data/nconf-import.sh
+++ data/nconf-import.sh
@@ -12,7 +12,7 @@ confdest=$2 # Importing from DESTINATION because we already imported the backup 
   rm -rf /var/lib/mysql/
   if (( $(awk 'BEGIN {print ("'$ver'" >= "'1.5'")}') )); then
     printf "Using v1.5 defaults... "
-    cp -R /root/nems/nems-migrator/data/1.4/mysql/NEMS-Clean /var/lib
+    cp -R /root/nems/nems-migrator/data/1.5/mysql/NEMS-Clean /var/lib
   elif (( $(awk 'BEGIN {print ("'$ver'" >= "'1.4'")}') )); then
     printf "Using v1.4 defaults... "
     cp -R /root/nems/nems-migrator/data/1.4/mysql/NEMS-Clean /var/lib
