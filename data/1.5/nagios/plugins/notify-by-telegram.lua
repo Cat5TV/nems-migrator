5afff6116336e36df89aba3b8709e61736c5553e
NEMS Linux <nems@baldnerd.com>
2019-09-05 16:11:50 -0400

    Telegram, sd* resizing, Docker

--- data/1.5/nagios/plugins/notify-by-telegram.lua
+++ data/1.5/nagios/plugins/notify-by-telegram.lua
@@ -4,7 +4,7 @@
 --   Kaganishu for helping with documentation and improvements for NEMS 1.5
 --   NickTheGreek for contributing his findings to help improve functionality for NEMS 1.5
 
--- VERSION 1.5.1
+-- VERSION 1.5.2
 
 https = require('ssl.https')
 local handle = io.popen("/usr/local/bin/nems-info alias")
@@ -13,6 +13,17 @@ handle:close()
 local nemsalias = string.gsub(result, "\n", "")
 local telegram_url = 'https://api.telegram.org/bot' .. arg[1] .. '/sendMessage?'
 local chat_id = '-' .. arg[2]:gsub('%g', '')
+
+-- UTF-8 Emojis Based on State
+if string.find(arg[5]:lower(), "crit") then
+  scolor = "🚫" -- No Entry Sign
+elseif string.find(arg[5]:lower(), "warn") then
+  scolor = "⚠️" -- Warning Sign
+elseif string.find(arg[5]:lower(), "unknown") then
+  scolor = "❓" -- Question Mark
+else
+  scolor = "✅" -- White Heavy Check Mark
+end
 
    Notification = "Notification Type: " .. arg[3] ..'\n'    --$NOTIFICATIONTYPE$
    Host = "Host: " .. arg[4] ..'\n'                         --$HOSTNAME$
@@ -23,10 +34,10 @@ local chat_id = '-' .. arg[2]:gsub('%g', '')
    Alias = "Reporting NEMS Server: " .. nemsalias .. '\n'   --NEMS Server Alias
 
    if (#arg == 8) then --assumes this since 8 arguments have been passed
-     message = '***** NEMS Linux ***** ' ..'\n' ..'\n' .. Notification .. Host ..State .. Address .. Info .. Date_Time .. Alias
+     message = scolor .. '\n' .. '\n' .. Notification .. Host ..State .. Address .. Info .. Date_Time .. Alias
    else 
      Service = "Service: " .. arg[8] .. '\n'                  --$SERVICEDESC$
-     message = '***** NEMS Linux ***** ' ..'\n' ..'\n' .. Notification .. Host  .. Service ..State .. Address .. Info .. Date_Time .. Alias
+     message = scolor .. '\n' .. '\n' .. '***** NEMS Linux ***** ' ..'\n' ..'\n' .. Notification .. Host  .. Service ..State .. Address .. Info .. Date_Time .. Alias
    end
 
 local data_str = 'chat_id=' .. chat_id .. '&text=' .. message..''  
